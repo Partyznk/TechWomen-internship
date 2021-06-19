2e594696531800def0a86445fcbe64b5fb43c19f
Bianca <bianca.raetchi@gmail.com>
2021-06-19 14:33:30 +0300

    8th app committed

---
+++ src/asterisk/Application.java
@@ -0,0 +1,15 @@
+package asterisk;
+
+public class Application {
+	public static void main(String[] args) {
+		System.out.print("*********     ***       *        *\n"			// Printing
+						+"*       *   *     *    ***      * *\n"
+						+"*       *  *       *  *****    *   *\n"
+						+"*       *  *       *    *     *     *\n"
+						+"*       *  *       *    *    *       *\n"
+						+"*       *  *       *    *     *     *\n"
+						+"*       *  *       *    *      *   *\n"
+						+"*       *   *     *     *       * *\n"
+						+"*********     ***       *        *");
+	}
+}
