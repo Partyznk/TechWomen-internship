0e2aa9c5b81da5403140cc336a912c28e696a1fb
Bianca <bianca.raetchi@gmail.com>
2021-06-26 16:15:56 +0300

    Checking for 5-digit integers added

--- src/palindrome/Application.java
+++ src/palindrome/Application.java
@@ -7,9 +7,13 @@ public class Application {
 		int number;						// Declaring variables
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Enter a 5-digit integer: ");		// Reading integer
+		System.out.print("Enter a 5-digit integer: ");		// Reading integer
 		number = input.nextInt();	
 																// Checking if the number is a palindrome
+		while (number / 10000 == 0 || number / 100000 != 0) {
+			System.out.print("ERROR: the integer is not a 5-digit integer. Please enter a 5-digit integer: ");
+			number = input.nextInt();
+		}
 		if (number % 10 == number / 10000 && number / 1000 % 10 == number % 100 /10) {
 			System.out.printf("%d is a palindrome", number);	// Print result
 		}
