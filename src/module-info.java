deb66e8bc98a3a0580c4440a2d950d70a2a6f78c
Bianca <bianca.raetchi@gmail.com>
2021-06-27 12:46:47 +0300

    18th app committed

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module product_of_integers {
+}
---
+++ src/product_of_integers/Application.java
@@ -0,0 +1,13 @@
+package product_of_integers;
+
+public class Application {
+	public static void main(String[] args) {
+		int i, p = 1;
+		
+		for(i = 1; i <= 15; i += 2) {
+			p *= i;
+		}
+		
+		System.out.println(p);
+	}
+}
